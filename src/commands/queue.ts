91651d727ba9598fd804210d2088ae8e464caa2f
Knoddy <cow2cow4cow@gmail.com>
2021-12-28 18:02:36 -0500

    Add queue command

---
+++ src/commands/queue.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+import { CommandData } from '../typings/CommandData';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('queue')
+    .setDescription('Sends the current queue.')
+    .toJSON(),
+
+  async execute(interaction: CommandInteraction, { player }: CommandData) {
+    const queueMessage = '**QUEUE:**';
+    
+    const queue = player.getQueue();
+    const nowIndex = player.getNowPlayingIndex();
+
+    for (const [index, song] of queue.entries()) {
+      queueMessage.concat(`\n${index === nowIndex ? `**${song.title}**` : song.title}`);
+    } 
+
+    return interaction.reply({ content: queueMessage, ephemeral: true });
+  }
+};
